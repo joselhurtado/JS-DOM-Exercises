494600a6b044f93d2c97e34f0ef4cd892dde31ea
Jose Hurtado <hello@hurtadojose.com>
2022-07-26 05:24:31 +0000

    Exercise 6 Complied

--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,3 +1,9 @@
-//you code here
+//Code Already Compiled
+
 document.body.innerHTML = "<h1>Hello World</h1>";
+
+const elem = document.createElement('img')
+elem.src  = "https://via.placeholder.com/350x150";
+document.body.appendChild(elem);
+
 
