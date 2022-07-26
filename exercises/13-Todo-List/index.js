31dcc7d4a21d90a898f22830d935e18a9cc5a658
Jose Hurtado <hello@hurtadojose.com>
2022-07-26 06:48:23 +0000

    Excercise 13 WIP

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,17 @@
-// Your code here
+// Code WIP
+let input = document.querySelector("#addToDo");
+input.addEventListener("keyup", addTodo);
+
+let ul = document.querySelector("ul");
+
+function addTodo(event) {
+    if(event.keyCode == 13){
+    let task = event.target.value;
+    ul.innerHTML += `<li><span><i class="fa fa-trash"></i></span>${task}</li>`;
+    console.log(event, "event");
+}
+}
+
+
+
+
