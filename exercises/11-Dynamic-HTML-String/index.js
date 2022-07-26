fe5f39e201a0213b92629431c94436aaad566f0b
Jose Hurtado <hello@hurtadojose.com>
2022-07-26 05:39:12 +0000

    Exercise 11 Compiled

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,2 +1,5 @@
-let myString = "<p>Hello!</p> <strong>My friend</strong>";
-document.write(myString);
+//Code already compiled
+let date = new Date();
+let year = date.getFullYear();
+let myString = "<p>Hello!</p> <strong>My friend</strong>, ";
+document.write(myString, "we are in the year ", year);
