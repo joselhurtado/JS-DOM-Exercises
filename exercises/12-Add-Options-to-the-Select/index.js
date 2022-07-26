de39d4192c3a4e47de123484967d5e36ba28b237
Jose Hurtado <hello@hurtadojose.com>
2022-07-26 06:04:12 +0000

    Exercise 12 Compiled

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,2 +1,11 @@
+//Code Already Compiled
+window.onload = function() {
+    console.log("The Page was Loaded...");
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
+
+let mySelect = document.querySelector('#mySelect');
+    for(let country of countries) {
+        mySelect.innerHTML += `<option value=" ">${country}</option>`;
+        }
+    };
